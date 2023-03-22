299e81e9218e6f67e3a30e1d29d870f6a239c46a
Pol Vallverdu <pvallverdualcaide@gmail.com>
2023-03-22 22:48:17 +0100

    publishing with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java")
+    id("maven-publish")
     // id("com.github.johnrengelman.shadow").version("7.1.2")
 }
 
 group = "dev.polv"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -20,4 +21,22 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "MediaExtractor"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("file://" + System.getenv("local_maven"))
+        }
+    }
 }
